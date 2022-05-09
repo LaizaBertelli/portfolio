6e4a866cefc4eabbc6ef3877986e754464db0486
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-09 14:57:07 -0300

    feat: create basic about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,17 @@
-const About = () => {
+import React from 'react';
 
-};
+function About() {
+  return (
+    <div className="about_main">
+      <section className="about_text_container">
+        <h1 className="about_title">QUEM É LAIZA?</h1>
+        <p className="about_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis magnam nesciunt totam ea voluptatem perferendis vel perspiciatis quae pariatur quaerat consequatur, sequi omnis distinctio esse, harum odit. Sapiente, obcaecati est!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis magnam nesciunt totam ea voluptatem perferendis vel perspiciatis quae pariatur quaerat consequatur, sequi omnis distinctio esse, harum odit. Sapiente, obcaecati est!</p>
+      </section>
+      <section className="about_image_container">
+        <img className="about_image" alt="placeholder" src="#" />
+      </section>
+    </div>
+  );
+}
 
 export default About;
