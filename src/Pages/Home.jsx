7c1d069c3e1589649a693141dca723d5ff3ce9a8
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-09 14:42:13 -0300

    feat: render Header on Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,12 @@
-const Home = () => {};
+import React from 'react';
+import Header from '../Components/Header';
+
+function Home() {
+  return (
+    <div className="home">
+      <Header />
+    </div>
+  );
+}
 
 export default Home;
