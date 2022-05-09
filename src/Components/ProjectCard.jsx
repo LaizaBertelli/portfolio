10ad23823e1bd615940de9b344991fd6c78cc8be
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-09 15:09:14 -0300

    feat: create basic project card component

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,3 +1,14 @@
-const ProjectCard = () => {};
+import React from 'react';
+
+function ProjectCard() {
+  return (
+    <div className="project_card_container">
+      <section className="project_card_preview_img" />
+      <section className="project_label_card">
+        <p>Project 1</p>
+      </section>
+    </div>
+  );
+}
 
 export default ProjectCard;
