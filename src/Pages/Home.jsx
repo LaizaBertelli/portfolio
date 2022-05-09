188f14b6ec4a1a31817f4d11116cddec1b049240
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-09 15:20:28 -0300

    feat: create basic home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 import Header from '../Components/Header';
+import ContactBtn from '../Components/ContactBtn';
+import Footer from '../Components/Footer';
+import About from './About';
+import Projects from './Projects';
 
 function Home() {
   return (
-    <div className="home">
+    <main className="main">
       <Header />
-    </div>
+      <div className="home">
+        <div className="home_headline">
+          <section className="headline_image_container" />
+          <section className="headline_text_container">
+            <div className="headline_title">
+              <h1>LAIZA BERTELLI</h1>
+              <h6>Desenvolvedora Front-end</h6>
+            </div>
+            <div className="social_media_links">
+              <AiFillGithub />
+              <AiFillLinkedin />
+            </div>
+            <ContactBtn />
+          </section>
+        </div>
+      </div>
+      <About />
+      <Projects />
+      <Footer />
+    </main>
   );
 }
 
