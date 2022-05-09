47d40584775c24e48870dd1147c82220d4c3f3be
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-09 19:35:50 -0300

    feat: add anchors

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import '../Styles/Pages/About.css';
+import placeholder from '../Assets/placeholder.svg';
 
 function About() {
   return (
     <div className="about_main">
-      <section className="about_text_container">
+      <section className="about_text_container" id="about">
         <h1 className="about_title">QUEM É LAIZA?</h1>
         <p className="about_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis magnam nesciunt totam ea voluptatem perferendis vel perspiciatis quae pariatur quaerat consequatur, sequi omnis distinctio esse, harum odit. Sapiente, obcaecati est!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis magnam nesciunt totam ea voluptatem perferendis vel perspiciatis quae pariatur quaerat consequatur, sequi omnis distinctio esse, harum odit. Sapiente, obcaecati est!</p>
       </section>
       <section className="about_image_container">
-        <img className="about_image" alt="placeholder" src="#" />
+        <img className="about_image" alt="placeholder" src={placeholder} />
       </section>
     </div>
   );
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import '../Styles/Pages/Projects.css';
 import ProjectCard from '../Components/ProjectCard';
 
 function Projects() {
   return (
-    <ProjectCard />
+    <div className="projects_main" id="projects">
+      <ProjectCard />
+    </div>
   );
 }
 
