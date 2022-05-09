55abefaf0208fcd149ee20ea165718b53e1f0de5
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-09 14:52:08 -0300

    feat: create basic contact component

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,3 +1,26 @@
-const Contact = () => {};
+import React from 'react';
+import SendBtn from '../Components/SendBtn';
+
+function Contact() {
+  return (
+    <div className="contact_main">
+      <h1 className="contact_me_title">Fale comigo</h1>
+      <label htmlFor="name_input">
+        Nome
+        <input className="name_input" name="name_input" type="text" />
+      </label>
+      <label htmlFor="email_input">
+        Email
+        <input className="email_input" name="email_input" type="email" />
+      </label>
+      <label htmlFor="message_input">
+        Menssagem
+        <textarea className="message_input" name="message_input" />
+      </label>
+
+      <SendBtn />
+    </div>
+  );
+}
 
 export default Contact;
