2278f1bd3080552e2d8698f3bfc9f84e28d4ce75
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-09 14:41:46 -0300

    feat: implement basic Header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,14 @@
-const Header = () => {
-};
+import '../Styles/Header.css';
+import React from 'react';
+import NavBar from './NavBar';
+
+function Header() {
+  return (
+    <div className="header">
+      <p>LAIZA</p>
+      <NavBar />
+    </div>
+  );
+}
 
 export default Header;
