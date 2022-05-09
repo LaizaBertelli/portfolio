8d07ae98445018db5cc23078db4a164670d67400
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-09 14:42:00 -0300

    feat: implement basic NavBar component

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,3 +1,16 @@
-const NavBar = () => {};
+import React from 'react';
+
+function NavBar() {
+  return (
+    <div className="nav_bar">
+      <ul className="nav_bar_list">
+        <li>home</li>
+        <li>sobre</li>
+        <li>projetos</li>
+        <li>contato</li>
+      </ul>
+    </div>
+  );
+}
 
 export default NavBar;
