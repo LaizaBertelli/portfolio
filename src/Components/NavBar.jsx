c1a807f4758e9ed6e21abb121135aff3aa6239cc
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-09 15:26:31 -0300

    feat: add links to the navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
     <div className="nav_bar">
       <ul className="nav_bar_list">
-        <li>home</li>
-        <li>sobre</li>
-        <li>projetos</li>
-        <li>contato</li>
+        <li><a href="#home">home</a></li>
+        <li><a href="#about">sobre</a></li>
+        <li><a href="#projects">projetos</a></li>
+        <li><Link to="contact">contato</Link></li>
       </ul>
     </div>
   );
