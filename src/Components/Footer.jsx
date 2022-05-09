acedb8ba5e49b1742a16e6b76d2c0a3351a3abf6
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-09 14:53:51 -0300

    feat: create basic footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,11 @@
-const Footer = () => {};
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="footer_main">
+      <p className="footer_text">Feito por Laiza Bertelli</p>
+    </div>
+  );
+}
 
 export default Footer;
