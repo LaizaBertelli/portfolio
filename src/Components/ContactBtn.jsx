9aa17a75ca92dfc39972b04db74a7eea7e36d56c
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-12 19:38:54 -0300

    feat: implement function to open/close modal

--- src/Components/ContactBtn.jsx
+++ src/Components/ContactBtn.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../Styles/Components/ContactBtn.css';
+import ContactContext from '../Context/ContactContext';
 
 function ContactBtn() {
+  const {
+    setIsModalOpen,
+  } = useContext(ContactContext);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
   return (
-    <button type="button" className="contact_button">
+    <button type="button" className="contact_button" onClick={handleOpenModal}>
       CONTATE-ME
     </button>
   );
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,29 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../Styles/Pages/Contact.css';
+import Modal from 'react-modal';
 import SendBtn from '../Components/SendBtn';
+import ContactContext from '../Context/ContactContext';
+
+Modal.setAppElement('#root');
 
 function Contact() {
+  const { isModalOpen, setIsModalOpen } = useContext(ContactContext);
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div className="contact_main">
-      <section className="contact_image" />
-      <section className="contact_forms">
-        <h1 className="contact_me_title">Fale comigo</h1>
-        <label htmlFor="name_input">
-          Nome
-          <input className="name_input" name="name_input" type="text" />
-        </label>
-        <label htmlFor="email_input">
-          Email
-          <input className="email_input" name="email_input" type="email" />
-        </label>
-        <label htmlFor="message_input">
-          Menssagem
-          <textarea className="message_input" name="message_input" />
-        </label>
-
-        <SendBtn />
-      </section>
-    </div>
+    <Modal
+      isOpen={isModalOpen}
+      onRequestClose={handleModalClose}
+      className="contact_modal"
+    >
+      <div className="contact_main">
+        <section className="contact_image" />
+        <section className="contact_forms">
+          <h1 className="contact_me_title">Fale comigo</h1>
+          <label htmlFor="name_input">
+            Nome
+            <input
+              className="name_input"
+              name="name_input"
+              type="text"
+            />
+          </label>
+          <label htmlFor="email_input">
+            Email
+            <input className="email_input" name="email_input" type="email" />
+          </label>
+          <label htmlFor="message_input">
+            Menssagem
+            <textarea className="message_input" name="message_input" />
+          </label>
+          <SendBtn />
+        </section>
+      </div>
+    </Modal>
   );
 }
 
